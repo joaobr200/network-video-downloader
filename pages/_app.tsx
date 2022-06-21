f82538734e423d8f37fa740e1c7d28bee7a7d5ab
joaobr200 <joaoarantesjob@gmail.com>
2022-06-21 17:37:25 -0300

    app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import myStore from "../store";
+import { globalStyles } from "../styles/stitches.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={myStore}>
+      <Component {...pageProps} />
+      {globalStyles()}
+    </Provider>
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "../styles/stitches.config";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <title>Network Video Downloander</title>
+          <meta
+            name="description"
+            content="Baixe qualquer vídeo do youtube, facebook, tiktok e outras networks, confira a lista em nosso site."
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
