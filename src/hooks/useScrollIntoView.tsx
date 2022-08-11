586763dfd69be70a39db3e3c952a92c27f194e1a
joaobr200 <joaoarantesjob@gmail.com>
2022-08-11 01:17:08 -0300

    fix: hook scroll into view

--- src/hooks/useScrollIntoView.tsx
+++ src/hooks/useScrollIntoView.tsx
@@ -1,11 +1,11 @@
-import { MutableRefObject, useCallback, useEffect } from "react";
+import { MutableRefObject, useEffect } from "react";
 
 export function useScrollIntoView<T extends HTMLElement | null>(
   ref: MutableRefObject<T>
 ) {
   useEffect(() => {
     ref.current?.scrollIntoView(true);
-  }, []);
+  }, [ref]);
 
   return;
 }
